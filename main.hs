f9a82886a57f7bde6d1c8c9cd54afa799c095446
MyUny <german.caggianese@proton.me>
2023-03-31 15:09:06 -0300

    update

--- main.hs
+++ main.hs
@@ -1,11 +1,21 @@
+import Control.Monad (unless)
+
 main :: IO ()
 main = do
-   putStrLn  "   _  __   __                     ____     _              "
-   putStrLn   "  | |/ /  / /_   _____  ____ _   / __ \\   (_)  _____  ___ "
-   putStrLn   "  |   /  / __/  / ___/ / __ `/  / / / /  / /  / ___/ / _ \\"
-   putStrLn  " /   |  / /_   / /    / /_/ /  / /_/ /  / /  / /__  /  __/"
-   putStrLn "/_/|_|  \\__/  /_/     \\__,_/  /_____/  /_/   \\___/  \\___/"
+  let i = "i"
 
-   input <- getLine
-   putStrLn $ "potaro " ++ input ++ " "
+--  i <- getLine
 
+  unless (i == "3") $ do
+    putStrLn  "   _  __   __                     ____     _              "
+    putStrLn   "  | |/ /  / /_   _____  ____ _   / __ \\   (_)  _____  ___ "
+    putStrLn   "  |   /  / __/  / ___/ / __ `/  / / / /  / /  / ___/ / _ \\"
+    putStrLn  " /   |  / /_   / /    / /_/ /  / /_/ /  / /  / /__  /  __/"
+    putStrLn "/_/|_|  \\__/  /_/     \\__,_/  /_____/  /_/   \\___/  \\___/"
+    putStrLn " "
+    putStrLn "1 - Roll a Dice"
+    putStrLn "2 - Load a File"
+    putStrLn "3 - Exit"
+    i <- getLine
+    main
+  putStrLn $ "potaro "
