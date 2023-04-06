fe50a7637606d01a9b0fed88c8d1c59c5837b044
MyUny <german.caggianese@proton.me>
2023-04-05 22:29:50 -0300

    Menu pseudo-finished

--- main.hs
+++ main.hs
@@ -2,20 +2,26 @@ import Control.Monad (unless)
 
 main :: IO ()
 main = do
-  let i = "i"
 
---  i <- getLine
+  putStrLn  "   _  __   __                     ____     _              "
+  putStrLn   "  | |/ /  / /_   _____  ____ _   / __ \\   (_)  _____  ___ "
+  putStrLn   "  |   /  / __/  / ___/ / __ `/  / / / /  / /  / ___/ / _ \\"
+  putStrLn  " /   |  / /_   / /    / /_/ /  / /_/ /  / /  / /__  /  __/"
+  putStrLn "/_/|_|  \\__/  /_/     \\__,_/  /_____/  /_/   \\___/  \\___/"
+  putStrLn " "
+  putStrLn "1 - Roll a Dice"
+  putStrLn "2 - Load a File"
+  putStrLn "3 - Exit"
+ 
+  i <- getLine
 
   unless (i == "3") $ do
-    putStrLn  "   _  __   __                     ____     _              "
-    putStrLn   "  | |/ /  / /_   _____  ____ _   / __ \\   (_)  _____  ___ "
-    putStrLn   "  |   /  / __/  / ___/ / __ `/  / / / /  / /  / ___/ / _ \\"
-    putStrLn  " /   |  / /_   / /    / /_/ /  / /_/ /  / /  / /__  /  __/"
-    putStrLn "/_/|_|  \\__/  /_/     \\__,_/  /_____/  /_/   \\___/  \\___/"
-    putStrLn " "
-    putStrLn "1 - Roll a Dice"
-    putStrLn "2 - Load a File"
-    putStrLn "3 - Exit"
-    i <- getLine
+    case i of
+      "1" -> putStrLn "caldo 1"
+      "2" -> putStrLn "caldo 2"
+      "4" -> caldo1
     main
-  putStrLn $ "potaro "
+
+caldo1 = do
+
+  putStrLn "salto caldoso"
